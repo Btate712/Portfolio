a1289fe87d3cbb0eeb04ca122bc6e025b08cee7a
btate712 <btate712@gmail.com>
2021-01-10 15:27:48 -0600

    added location (href) to cards to link to project pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,17 @@ const Card = props => {
   const [ hasFocus, setHasFocus ] = useState(false);
 
   return(
-    <div 
-      className="Card"
-      onMouseEnter={ () => setHasFocus(true) }
-      onMouseLeave={ () => setHasFocus(false) }
-    >
-      <p className={ hasFocus ? "focus" : "unfocus" }>
-        { hasFocus ? props.description : props.word }
-      </p>
-    </div>
+    <a href={ props.location }>
+      <div 
+        className="Card"
+        onMouseEnter={ () => setHasFocus(true) }
+        onMouseLeave={ () => setHasFocus(false) }
+      >
+        <p className={ hasFocus ? "focus" : "unfocus" }>
+          { hasFocus ? props.description : props.word }
+        </p>
+      </div>
+    </a>
   )
 }
 
--- src/containers/Cards.js
+++ src/containers/Cards.js
@@ -5,8 +5,16 @@ import '../styles/Cards.css';
 const Cards = props => {
   return (
     <div className='Cards'>
-      <Card word="2048 Clone" description="A clone of the game 2048 written in vanilla JS" />
-      <Card word="Bubble Sort Visualizer" description="A vanilla JS app written to visualize how a bubble sort works" />
+      <Card 
+        word="2048 Clone" 
+        description="A clone of the game 2048 written in vanilla JS" 
+        location="http://www.thetater.net/game2448.html"
+      />
+      <Card 
+        word="Bubble Sort Visualizer" 
+        description="A vanilla JS app written to visualize how a bubble sort works" 
+        location="http://www.thetater.net/bubblesort/bubblesort.html"
+      />
     </div>
   )
 }
