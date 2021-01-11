8a075b2ec47d965455583c968c73cd358c740788
btate712 <btate712@gmail.com>
2021-01-11 14:51:43 -0600

    added a basic welcome message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Cards from './containers/Cards';
 import MyHeader from './components/MyHeader';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <MyHeader />
+      <Welcome />
       <Cards />
     </div>
   );
---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Welcome.css";
+
+const Welcome = props => {
+  return( 
+    <div className="Welcome" >
+      <h1>Welcome to Bob Tate's Portfolio Site!</h1>
+      <p>Please hover over a project for more info and click to launch the project in a new tab.</p>
+    </div>
+  )
+}
+
+export default Welcome;
