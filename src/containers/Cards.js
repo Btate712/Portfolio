790fd507675c232d2b1e5a2eae3380e67503998f
btate712 <btate712@gmail.com>
2021-01-11 14:25:54 -0600

    moved projects into data file

--- src/containers/Cards.js
+++ src/containers/Cards.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Card from '../components/Card';
 import '../styles/Cards.css';
+import portfolio from '../data/portfolio.js'
 
 const Cards = props => {
+  const mapProjects = () => {
+    return portfolio.map(project => {
+      return(
+        <Card
+          word={ project.title }
+          description={ project.description }
+          location={ project.location }
+        />
+      );
+    });
+  }
+
   return (
     <div className='Cards'>
-      <Card 
-        word="2048 Clone" 
-        description="A clone of the game 2048 written in vanilla JS" 
-        location="http://www.thetater.net/game2448.html"
-      />
-      <Card 
-        word="Bubble Sort Visualizer" 
-        description="A vanilla JS app written to visualize how a bubble sort works" 
-        location="http://www.thetater.net/bubblesort/bubblesort.html"
-      />
+      { mapProjects() }
     </div>
   )
 }
---
+++ src/data/portfolio.js
@@ -0,0 +1,13 @@
+const portfolio = [
+  {
+    "title": "2048 Clone",
+    "description": "A clone of the game 2048 written in vanilla JS",
+    "location": "http://www.thetater.net/game2448.html"
+  },{
+    "title": "Bubble Sort Visualizer",
+    "description": "A vanilla JS app written to visualize how a bubble sort works" ,
+    "location": "http://www.thetater.net/bubblesort/bubblesort.html"
+  }
+]
+
+export default portfolio;
