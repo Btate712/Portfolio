9cd91c130dc8b3333ee9939f56bf42d0e5e289bf
btate712 <btate712@gmail.com>
2021-01-12 07:45:32 -0600

    Set welcome message depending on whether user is on mobile

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "../styles/Welcome.css";
 
-const Welcome = props => {
+const Welcome = ({ isMobile }) => {
   return( 
     <div className="Welcome" >
       <h1>Welcome to Bob Tate's Portfolio Site!</h1>
-      <p>Please hover over a project for more info and click to launch the project in a new tab.</p>
+      <p>Please { isMobile ? "click on" : "hover over" } a project for more info and click { isMobile ? "the launch button" : ""} to launch the project in a new tab.</p>
     </div>
   )
 }
