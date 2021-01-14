68f4a34836b1a73ae2241c82b93d4b29b8225095
btate712 <btate712@gmail.com>
2021-01-14 15:41:43 -0600

    experimenting with contact page styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import "../styles/Contact.css";
 
 const Contact = props => {
   return (
     <div className="Contact">
-      <h1>Contact Page</h1>
+      <h1>Contact Information</h1>
+      <p>
+        Please enter your message into the fields below.
+      </p>
+      <label htmlFor="subject">
+        Subject: <input type="text" name="subject"></input>
+      </label>
+      <br />
+      <label htmlFor="message">
+        Message: <br />
+        <textarea name="message" cols="80" rows="20"/>
+      </label>
+      <br />
+      <Link className="link" id="send">Send</Link>
+      <Link className="link" id="home" to="/">Home</Link>
     </div>
   );
 }
